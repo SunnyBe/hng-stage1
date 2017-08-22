3823353db230682c51497f102344d69058cb5be4
Ndu Sunday <ndusunday@gmail.com>
2017-08-22 09:05:12 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
-<?php
-include_once "config/db-config.php";
-include_once "config/Database.php";
-include_once "model/User.php";
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Ndu Sunday Profile</title>
+		<link rel="stylesheet" type="text/css" href="customize.css">
 
-$user = User::getByUsername('michaels');
-?>
-<p><b>Username: </b><?php echo $user->getUsername();?></p>
-<p><b>Email: </b><?php echo $user->getEmail();?></p>
+	</head>
+	<body>
+		<div class = "main">
+
+			<h1>
+				<P>NDU SUNDAY ZISTUS</P>
+				<a href="#"><img width="400" height="260" alt="Profile Pics" src="myProfile.png"></a>
+				<br>
+				Software Engineer
+			</h1>
+			<ul class="head_list">
+				<h3>slack: <a href="#">ndusunday</a></h3>
+
+				<div class="about">
+				<h4 class="about-head">About Me</h4>
+					<div class="about-content">
+						<p>I love working with backends majorly php but am heading to be fullstack, i also do android developement and also desktop development using JavaFx.</p>
+						<p> I love innovation and can put my self on the line to see innovation born</p>
+						
+					</div>		
+				</div>
+				<h3><a href="htpps://github.com/SunnyBe/hng">Git</a></h3>
+				<h3><a href="https://www.facebook.com/sunday.ndu.96">Facebook</a></h3>
+				<h3><a href="https://twitter.com/ndusundayDev">Twitter</a></h3>
+			</ul>
+		</div>
+
+	</body>
+
+<html>
